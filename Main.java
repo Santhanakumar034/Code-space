add94b99ff045a771d97814a81d8aa09e0a5f5fc
Santhanakumar034 <110150164+Santhanakumar034@users.noreply.github.com>
2025-09-07 00:00:02 +0530

    Revert "Created the homepage for UPI Banking App. Added Main.java and HomePage.java"

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        HomePage homepage=new HomePage(); //Object is created for homepage
-        homepage.showMenu(); //Call method 
-    }
-}
