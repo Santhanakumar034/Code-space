a727a0142660295f33a77197955f699f3c479fb8
Santhanakumar034 <110150164+Santhanakumar034@users.noreply.github.com>
2026-07-18 18:53:13 +0530

    Create Datatypes.java

---
+++ Java Programs/Datatypes.java
@@ -0,0 +1,14 @@
+class Datatypes{
+  public static void main (String [] args){
+    int a = 10;
+    float f = 4.5f;
+    double d = 29907384.02;
+    char c = 'a';
+    byte b = 1;
+        System.out.println("a is int");
+        System.out.println("b is byte");
+        System.out.println("c is char");
+        System.out.println("d is double");
+        System.out.println("f is float");
+  }
+}
